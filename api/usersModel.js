50ecf8f80f8dbedb4eba42524c796ea7c39082b0
ericbanker12 <ericbanker12@gmail.com>
2019-11-14 17:09:46 -0600

    create add method

--- api/usersModel.js
+++ api/usersModel.js
@@ -2,9 +2,15 @@ const db = require('../data/db')
 
 module.exports = {
     find,
+    add,
 }
 
 function find(filter) {
     if (filter) return db('users').where(filter).first()
     else return db('users')
+}
+
+function add(user) {
+    return db('users').insert(user, '*')
+        .then(([id]) => find({id}))
 }
