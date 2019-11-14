0ac1f7b83b196405588400a333395de9004dffc4
ericbanker12 <ericbanker12@gmail.com>
2019-11-14 17:37:33 -0600

    create tests for post /users

--- api/server.test.js
+++ api/server.test.js
@@ -1,8 +1,10 @@
 const request = require('supertest')
 
 const server = require('./server')
+const db = require('../data/db')
 
 describe('/API', () => {
+
     describe('GET /users', () => {
         
         test('should receive status 200 OK', () => {
@@ -20,6 +22,31 @@ describe('/API', () => {
             return request(server).get('/api/users')
                 .then(resp => {
                     expect(Array.isArray(resp.body)).toBe(true)
+                })
+        })
+    })
+
+    describe('POST /users', () => {
+
+        beforeEach(() => db('users').truncate())
+        
+        test('should receive status 201 Created', () => {
+            return request(server).post('/api/users', {username: 'testing'}).expect(201)
+        })
+
+        test('should receive content type json', () => {
+            return request(server).post('/api/users', {username: 'testing'})
+                .then(resp => {
+                    expect(resp.type).toMatch(/json/i)
+                })
+        })
+
+        test('should receive added user with an id', () => {
+            return request(server).post('/api/users', {username: 'testing'})
+                .then(resp => {
+                    expect(resp.body).toBeDefined()
+                    expect(resp.body.username).toBe('testing')
+                    expect(resp.body.id).toBeDefined()
                 })
         })
     })
