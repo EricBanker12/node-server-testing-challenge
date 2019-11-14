76ace9c32721b134865673e65631c701dc10d262
ericbanker12 <ericbanker12@gmail.com>
2019-11-14 17:27:22 -0600

    update /users endpoint

--- api/server.js
+++ api/server.js
@@ -2,16 +2,25 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const usersDB = require('./usersModel')
+
 const server = express()
 
 // middleware
-// server.use(helmet())
-// server.use(cors())
-// server.use(express.json())
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
 
 // routes/routers
 server.get('/api/users', (req, res) => {
-    res.json([])
+    usersDB.find()
+        .then(resp => {
+            res.json(resp)
+        })
+        .catch(err => {
+            console.error(err)
+            res.sendStatus(500)
+        })
 })
 
 module.exports = server
